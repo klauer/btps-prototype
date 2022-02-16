88eece9625837a98a68c67ed8197bc1536eb9ee6
Ken Lauer <klauer@slac.stanford.edu>
2022-02-16 12:01:09 -0800

    MNT: rebuild ioc

--- iocBoot/ioc-Untitled1/load_plc_databases.cmd
+++ iocBoot/ioc-Untitled1/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 2903
-callbackSetQueueSize(7806)
+# Total records: 3199
+callbackSetQueueSize(8398)
 dbLoadRecords("Untitled1.db", "PORT=ASYN_PLC,SIM=,BTPS=SIM:BTPS,PREFIX=TST:BTPS::,IOCNAME=$(IOC),IOC=$(IOC)")
